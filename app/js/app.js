0abd350a43777566b7ca2f63f946df55f5feb0f9
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2018-03-23 19:43:17 +0200

    update

---
+++ app/js/app.js
@@ -0,0 +1,46 @@
+
+const App = ((Canvas, Util) => {
+    const fps       = 1000 / 60;
+
+    const canvas    = new Canvas('#canvas')
+        , context   = canvas.context
+        , center    = canvas.center;
+
+    const mouse     = {
+        x       : 0,
+        y       : 0,
+        angle   : 0 
+    }
+
+    const triangle  = {
+        a       : 100,
+        h       : 200,
+        color   : '#fff'
+    }; 
+
+    const coords = Util.calculateTriangeCoords(center, triangle.a, triangle.h);
+
+    const mouseMoveListener = (e) => {
+        mouse.x     = e.clientX - canvas.offsetLeft;
+        mouse.y     = e.clientY - canvas.offsetTop;
+        mouse.angle = Util.calculateAngle(canvas.center, mouse);
+
+        document.getElementById('x').innerHTML = mouse.x;
+        document.getElementById('y').innerHTML = mouse.y;
+        document.getElementById('angle').innerHTML = mouse.angle;
+    };
+
+    return {
+        init() {
+            canvas.on('mousemove', mouseMoveListener);
+
+            setInterval(() => {
+                canvas.clear();
+                canvas.drawTriangle(coords, triangle.color, mouse.angle);
+            }, fps);
+        }
+    };
+})(Canvas, Util);
+
+App.init();
+
